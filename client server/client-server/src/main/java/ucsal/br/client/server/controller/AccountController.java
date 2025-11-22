7c479f2cb7a11bb91173bcbd2a3be7df0c3da1df
sonliacharo <rocha7andrade@gmail.com>
2025-11-22 11:30:57 -0300

    client-server

---
+++ client server/client-server/src/main/java/ucsal/br/client/server/controller/AccountController.java
@@ -0,0 +1,30 @@
+package ucsal.br.client.server.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/account")
+@RefreshScope
+public class AccountController {
+
+    @Value("${msg:Default Message}")
+    private String message;
+
+    @Value("${taxa:0.0}")
+    private Double taxa;
+
+    @GetMapping("/info")
+    public Map<String, Object> getInfo() {
+        Map<String, Object> dados = new HashMap<>();
+        dados.put("mensagem", message);
+        dados.put("taxa_juros", taxa);
+        return dados;
+    }
+}
